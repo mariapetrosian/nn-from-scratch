bf463032e90cda1e8b51261c880c63042060b283
mariapetrosyann <mariaapetrosian@gmail.com>
2025-01-28 00:06:45 +0400

    Initial commit

--- app/server.py
+++ app/server.py
@@ -5,7 +5,8 @@ import numpy as np
 from typing import Dict
 from app.NeuralNetwork import NeuralNetwork
 
-model: joblib.Memory = joblib.load('app/model.joblib')
+model: NeuralNetwork = joblib.load('app/model.joblib')
+
 class_names: np.ndarray = np.array(['1', '0'])
 
 class PredictionRequest(BaseModel):
@@ -15,12 +16,13 @@ app: FastAPI = FastAPI()
 
 @app.get('/')
 def read_root() -> Dict[str, str]:
-    return {'message': 'Heart disease risk model API'}
+    return {"version": "Heart-disease detector 0.1", "status": "OK"}
 
 @app.post('/predict')
-def predict(data: PredictionRequest):
+def predict(data: PredictionRequest) -> Dict[str, float]:
     features = np.array(data.features).reshape(1, -1)
-    prediction = model.predict(features) 
-    class_name = int(prediction[0][0])
-    return {'predicted_class': class_name}
-
+    prediction = model.predict(features)
+    raw_output = model.forward(features)
+    predicted_class = int(prediction[0][0])
+    predicted_probability = raw_output[0][predicted_class]
+    return {'predicted_class': predicted_class, 'predicted_probability': predicted_probability}
